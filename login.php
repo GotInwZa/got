85d8694d647fb252ffc623746f14aa0ac87651cc
66209010028-rgb <66209010028@bncc.ac.th>
2026-02-19 13:26:54 +0700

    login

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
             // เช็ค role
             if ($row["role"] === "admin") {
-                header("Location: /admin");
+                header("Location: /admin/admin_user.php");
                 exit;
             } elseif ($row["role"] === "user") {
                 header("Location: /user");
