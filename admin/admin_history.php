1cb9464a73de8b3db8f2794b0cd6de6286baeafd
66209010028-rgb <66209010028@bncc.ac.th>
2026-02-19 16:29:54 +0700

    login

--- admin/admin_history.php
+++ admin/admin_history.php
@@ -3,6 +3,10 @@ require_once '../config/connectdb.php';
 require_once '../nav.php';
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /* ====== เช็คสิทธิ์ ====== */
 if (!isset($_SESSION["login"]) || $_SESSION["login"] !== true) {
     header(header: "Location: ../login.php");
