238b938a5a3b20398cbe20b86376cae180660772
66209010028-rgb <66209010028@bncc.ac.th>
2026-02-19 13:41:38 +0700

    wow

--- login.php
+++ login.php
@@ -33,10 +33,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
             // เช็ค role
             if ($row["role"] === "admin") {
-                header("Location: admin/admin_user.php");
+                header("Location: got/admin/admin_user.php");
                 exit;
             } elseif ($row["role"] === "user") {
-                header("Location: user");
+                header("Location: got/user/books.php");
                 exit;
             }
         } else {
