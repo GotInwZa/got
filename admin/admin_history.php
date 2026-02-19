186e65d40a2296c907a4d83bf978a9b3c1ac7f32
66209010028-rgb <66209010028@bncc.ac.th>
2026-02-19 17:05:53 +0700

    login

--- admin/admin_history.php
+++ admin/admin_history.php
@@ -9,9 +9,9 @@ if (!isset($_SESSION["login"]) || $_SESSION["login"] !== true) {
     exit;
 }
 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $role = $_SESSION['role'];
 $user_id = $_SESSION['user_id'];
