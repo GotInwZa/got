0e5896880617fc85df66d0ae0a32646567d40a22
gotinwza <nattapon2185@gmail.com>
2026-02-25 01:02:39 +0700

    update soon

--- admin/admin_create_user.php
+++ admin/admin_create_user.php
@@ -9,9 +9,9 @@ if (!isset($_SESSION["login"]) || $_SESSION["login"] !== true || $_SESSION["role
     exit;
 }
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 $error = "";
 $success = "";
@@ -47,7 +47,7 @@ if (isset($_POST['save'])) {
                            VALUES (?, ?, ?, ?)";
             $stmt_insert = $conn->prepare($sql_insert);
             $stmt_insert->bind_param(
-                "ssss",
+                "sssi",
                 $username,
                 $hash_password,
                 $hostname,
